45f18a478a1674d51beb8d65350d2059cbecfe12
Yousaf Nabi <you@saf.dev>
2022-08-22 00:06:39 +0100

    floop:

--- katacoda-hitokiri/cdc-with-k8s/init-background.sh
+++ katacoda-hitokiri/cdc-with-k8s/init-background.sh
@@ -23,8 +23,8 @@ curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share
     sudo apt-get install apt-transport-https --yes \
     echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" >/dev/null |
     sudo tee /etc/apt/sources.list.d/helm-stable-debian.list \
-        sudo apt-get update \
-        sudo apt-get install helm
+        sudo apt-get update -y \
+        sudo apt-get install -y helm
 
 curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
 source ~/.bashrc
