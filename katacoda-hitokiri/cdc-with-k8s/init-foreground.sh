1ec06b889f65c8c682174f2d18b5cf1825dd69ab
Yousaf Nabi <you@saf.dev>
2022-08-21 23:09:01 +0100

    ci:dontclear

--- katacoda-hitokiri/cdc-with-k8s/init-foreground.sh
+++ katacoda-hitokiri/cdc-with-k8s/init-foreground.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-FREQUENCY=1                                          # Delay between each check for completion
-BACKGROUND_SIGNAL_FILE='/opt/.backgroundfinished'    # File updated by background to indicate completion
-BACKGROUND_SAFE_WORD='done'                          # Word in BACKGROUND_SIGNAL_FILE indicating completion
-START_MESSAGE='Starting scenario'                    # Message before the progress animation
+FREQUENCY=1                                       # Delay between each check for completion
+BACKGROUND_SIGNAL_FILE='/opt/.backgroundfinished' # File updated by background to indicate completion
+BACKGROUND_SAFE_WORD='done'                       # Word in BACKGROUND_SIGNAL_FILE indicating completion
+START_MESSAGE='Starting scenario'                 # Message before the progress animation
 END_NORMAL_MESSAGE='Scenario ready. You have a running Kubernetes cluster.'
 END_KILLED_MESSAGE='Interupted. This scenario may still be initializing.'
 
-SPINNER_COLOR_NUM=2                # Color to use, unless COLOR_CYCLE=1
-SPINNER_COLOR_CYCLE=0              # 1 to rotate colors between each animation
+SPINNER_COLOR_NUM=2   # Color to use, unless COLOR_CYCLE=1
+SPINNER_COLOR_CYCLE=0 # 1 to rotate colors between each animation
 INTERVAL=.25
-SPINNER_NORMAL=$(tput sgr0)        # Reset encoding
+SPINNER_NORMAL=$(tput sgr0) # Reset encoding
 symbols=("▐⠂       ▌" "▐⠈       ▌" "▐ ⠂      ▌" "▐ ⠠      ▌" "▐  ⡀     ▌" "▐  ⠠     ▌" "▐   ⠂    ▌" "▐   ⠈    ▌" "▐    ⠂   ▌" "▐    ⠠   ▌" "▐     ⡀  ▌" "▐     ⠠  ▌" "▐      ⠂ ▌" "▐      ⠈ ▌" "▐       ⠂▌" "▐       ⠠▌" "▐       ⡀▌" "▐      ⠠ ▌" "▐      ⠂ ▌" "▐     ⠈  ▌" "▐     ⠂  ▌" "▐    ⠠   ▌" "▐    ⡀   ▌" "▐   ⠠    ▌" "▐   ⠂    ▌" "▐  ⠈     ▌" "▐  ⠂     ▌" "▐ ⠠      ▌" "▐ ⡀      ▌" "▐⠠       ▌")
 
 progress_pid=0
 
-cleanup () {
+cleanup() {
   kill $progress_pid >/dev/null 2>&1
   progress_pid=-1
   end_message=$END_KILLED_MESSAGE
 }
 
-show_progress () {  
+show_progress() {
   while :; do
     tput civis
     for symbol in "${symbols[@]}"; do
@@ -29,7 +29,7 @@ show_progress () {
         if [ $SPINNER_COLOR_NUM -eq 7 ]; then
           SPINNER_COLOR_NUM=1
         else
-          SPINNER_COLOR_NUM=$((SPINNER_COLOR_NUM+1))
+          SPINNER_COLOR_NUM=$((SPINNER_COLOR_NUM + 1))
         fi
       fi
       local color
@@ -44,7 +44,7 @@ show_progress () {
   return 0
 }
 
-start_progress () {
+start_progress() {
   show_progress &
   progress_pid=$!
 
@@ -56,7 +56,7 @@ start_progress () {
   # Periodically check for background signal or user Ctrl-C interuption
   end_message=$END_NORMAL_MESSAGE
   while [[ $progress_pid -ge 0 ]]; do
-    grep -i ${BACKGROUND_SAFE_WORD} ${BACKGROUND_SIGNAL_FILE} &> /dev/null
+    grep -i ${BACKGROUND_SAFE_WORD} ${BACKGROUND_SIGNAL_FILE} &>/dev/null
     if [[ "$?" -eq 0 ]]; then
       kill $progress_pid >/dev/null 2>&1
       break
@@ -64,11 +64,12 @@ start_progress () {
     sleep ${FREQUENCY}
   done
 
-  stty sane; tput cnorm; clear
+  stty sane
+  tput cnorm
   printf "%s\n\n" "${end_message}"
-  
+
   # Pick up any changes during background
   source ~/.bashrc
 }
 
-clear && start_progress
+start_progress
