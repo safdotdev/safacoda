2ff4a9477787909e5481fd6fb942832febb5fc7c
Yousaf Nabi <you@saf.dev>
2022-11-02 17:48:59 +0000

    ci:ruby

--- safacoda/pact-build-your-own-ffi-impl/intro-start.sh
+++ safacoda/pact-build-your-own-ffi-impl/intro-start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-echo "setup node 16"
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash && source ~/.bashrc && nvm install 16
+
 echo "Downloading pact_ruby_ffi"
 echo "=> downloading consumer project"
 git clone https://github.com/YOU54F/pact-ruby-ffi
 cd pact-ruby-ffi
+echo "setup ruby 2.7.6"
 apt update && \
 apt --yes install curl g++ gcc autoconf automake bison libc6-dev \
         libffi-dev libgdbm-dev libncurses5-dev libsqlite3-dev libtool \
@@ -13,4 +13,4 @@ apt --yes install curl g++ gcc autoconf automake bison libc6-dev \
         curl -sSL https://rvm.io/mpapis.asc | gpg --import - && \
 curl -sSL https://rvm.io/pkuczynski.asc | gpg --import - && \
 curl -sSL https://get.rvm.io | bash -s stable && \
-source ~/.rvm/scripts/rvm && rvm install 2.7.6
+source /etc/profile.d/rvm.sh && rvm install 2.7.6
