a25ec030662106f833bd9277cfaea46036a8e24e
Yousaf Nabi <you@saf.dev>
2022-11-02 19:02:38 +0000

    ci:ruby

--- safacoda/pact-build-your-own-ffi-impl/step1-verify.sh
+++ safacoda/pact-build-your-own-ffi-impl/step1-verify.sh
@@ -1,6 +1,2 @@
-#!/bin/bash
+!/bin/bash
 [ -f pact/ffi/linuxx8664/libpact_ffi.so  ] && echo "done"
-# [ -f pacts/Consumer-Alice Service.json  ] && echo "done"
-# [ -f pacts/http-consumer-2-http-provider.json ] && echo "done"
-# [ -f pacts/message-consumer-2-message-provider.json  ] && echo "done"
-# [ -f pacts/http-consumer-1-http-provider.json  ] && echo "done"
