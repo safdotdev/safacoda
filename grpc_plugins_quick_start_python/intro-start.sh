7472ce9d76251da5b6bccd103f41e501f5ee1963
Yousaf Nabi <you@saf.dev>
2022-11-25 17:33:34 +0000

    chore: move to top level to test scenario embed

---
+++ grpc_plugins_quick_start_python/intro-start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "=> Downloading pact-python-ffi"
+git clone https://github.com/YOU54F/pact-python-ffi
+cd ~/pact-python-ffi
+echo "setup python 3.10"
+apt update && \
+apt --yes install jq && \
+clear && echo "Welcome. The repo is downloaded, you are good to go!"
