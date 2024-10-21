eb9c705fc0ff3c8578fa3815c4553cabc3b1577a
Yousaf Nabi <you@saf.dev>
2024-10-21 15:13:04 +0100

    message(java) - add init tutorial

---
+++ scratchpad/pactflow-message-java/finish.md
@@ -0,0 +1,5 @@
+## Tada
+
+That is it for now! Thanks
+
+1. `exit`{{exec}}
---
+++ scratchpad/pactflow-message-java/intro-start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# echo "Installing apt dependencies"
+# apt --yes install jq maven && \
+# echo "setup node 20" && \
+# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash && source ~/.bashrc && nvm install 20 && \
+# echo "Installing OpenJDK 11" && \
+# mkdir -p /usr/java && \
+# cd /usr/java && \
+# wget -c https://download.java.net/java/GA/jdk17.0.2/dfd4a8d0985749f896bed50d7138ee7f/8/GPL/openjdk-17.0.2_linux-x64_bin.tar.gz && \
+# tar -xf openjdk-17.0.2_linux-x64_bin.tar.gz && \
+# export JAVA_HOME="/usr/java/jdk-11" && \
+# export PATH="/usr/java/jdk-11/bin:${PATH}" && \
+# cd ~ && \
+# clear && echo "Welcome. The repo is downloaded, dependencies are installed, you are good to go."
